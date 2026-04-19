48d26b69b29ccf4a596d0ae8b77cd78c21b8faf0
Yongyi Zhang <yongyizhang41@gmail.com>
2026-04-19 16:06:25 -0500

    fix nested repo for ice-webdevbasics1

---
+++ In_Class_Exercise/ice-webdevbasics1/starter/index.js
@@ -0,0 +1,9 @@
+// This is where your JS goes!
+
+const BASE_AMNS = [1, 15, 14.5, 2, 1, 1, 1]
+const REVIEWS = [
+    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
+    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
+    "Loved the hearty texture and rich taste - a new family favorite!",
+    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
+]
